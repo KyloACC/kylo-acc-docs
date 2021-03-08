1d53c9d8324d29db9c4fe314cf91b7822dba5b40
Kylo <kyloren@vivaldi.net>
2021-03-09 00:48:37 +0100

    Ferrari Spa fixed rear at slow speeds sort of and fucked with the fuel calc

--- fuelcalc.ps1
+++ fuelcalc.ps1
@@ -1,6 +1,6 @@
 $fuel = Read-Host -Prompt 'Input fuel per lap here'
 [string]$time = Read-Host -Prompt 'Input lap time here'
-$maxtime = Read-Host -Prompt 'Input race length here'
+[int]$maxtime = Read-Host -Prompt 'Input race length here'
 
 [int]$minute = $time.Substring(0,1)
 [int]$second = $time.Substring(2,2)
@@ -9,6 +9,7 @@ $time2 = ($minute * 60 + $second)
 
 [decimal] $fuel_per_minute = [math]::round($fuel / $time2 * 60,3)
 
-$result = [math]::round($fuel_per_minute * $maxtime + $fuel + $fuel)
+$laps = [math]::ceiling( (($maxtime*60) / $time2))
+$result = [math]::round([math]::ceiling($fuel_per_minute * $maxtime) + $fuel)
 
-" $result "
+" $result litres of fuel for $laps laps and one extra lap."
