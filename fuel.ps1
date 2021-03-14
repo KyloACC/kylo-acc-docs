28be13f071d69d24b1bd56980e98f6038ac88104
Kylo <kyloren@vivaldi.net>
2021-03-14 14:31:18 +0100

    Changed Porsche @ Spa, McLaren @ Donington with a 1:27.027 and made fuel calc write to file

--- fuel.ps1
+++ fuel.ps1
@@ -1,6 +1,8 @@
+$Car = Read-Host -Prompt 'Input Car here'
 $track = Read-Host -Prompt 'Input track name here'
 $fuel = Read-Host -Prompt 'Input fuel per lap here'
 $time = Read-Host -Prompt 'Input lap time in seconds here'
+
 
 [int]$minute = $time.Substring(0,1)
 [int]$second = $time.Substring(2,2)
@@ -13,3 +15,7 @@ $fuel2 = [math]::Floor($fuel_per_minute * 45) + 6
 $fuel3 = [math]::Floor($fuel_per_minute * 60) + 6
 
 "| $track 	| $fuel 		| $fuel_per_minute 		| $time 			| $fuel1 		| $fuel2		 | $fuel3 		|"
+
+"$Car 
+| $track 	| $fuel 		| $fuel_per_minute 		| $time 			| $fuel1 		| $fuel2		 | $fuel3 		|" | Out-File -FilePath 'C:\Users\lucaz\Documents\Assetto Corsa Competizione\Setups\fuel_calc.txt' -Append
+
