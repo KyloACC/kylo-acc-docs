f4cba3dd1a02722883d22fb44d0744d02f9307a4
Kylo <kyloren@vivlaid.net>
2021-12-12 23:28:14 +0100

    Update ConvertToMp3.ps1

--- ConvertToMp3.ps1
+++ ConvertToMp3.ps1
@@ -1,3 +1,3 @@
 [string]$inputfile = Read-Host -Prompt 'Input file?'
 [string]$outputfile = Read-Host -Prompt 'Output file?'
-ffmpeg -i $inputfile -vn -ab 256 $outputfile
+ffmpeg -i $inputfile -vn -ab 256k $outputfile
