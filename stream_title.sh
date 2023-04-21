e5af76737cabcbbef8a0a1d98dd985e6428c74f1
Kylo <kyloren@vivlaid.net>
2023-04-21 14:58:22 +0200

    Porsche Kyalami, Laguna Seca, Nürburgring + Imola

--- stream_title.sh
+++ stream_title.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
+#!/usr/bin/python3
 
 # Generate a random number between 1 and 7
 choice=$((1 + RANDOM % 7))
 
 case $choice in
-    1) 
-       echo "Welcome to the LFM Race!" 
+    1)
+       echo "Welcome to the LFM Race!"
        echo "Series Name?"; read SeriesName;
        echo "Track?"; read Track;
        echo "Car?"; read Car;;
-    2) 
-       echo "Car is ready to take on the Track in this LFM Race!" 
+    2)
+       echo "Car is ready to take on the Track in this LFM Race!"
        echo "Car?"; read Car;
        echo "Track?"; read Track;;
-    3) 
-       echo "Join us for an LFM Race and let's have some fun!" 
+    3)
+       echo "Join us for an LFM Race and let's have some fun!"
        echo "Car?"; read Car;
        echo "Track?"; read Track;
        echo "Series Name?"; read SeriesName;;
-    4) 
-       echo "Get ready for the LFM Race!" 
+    4)
+       echo "Get ready for the LFM Race!"
        echo "Car?"; read Car;
        echo "Track?"; read Track;;
-    5) 
-       echo "Rev up your engines for the LFM Race!" 
+    5)
+       echo "Rev up your engines for the LFM Race!"
        echo "Car?"; read Car;
        echo "Track?"; read Track;;
-    6) 
-       echo "It's time for the LFM Race!" 
+    6)
+       echo "It's time for the LFM Race!"
        echo "Car?"; read Car;
        echo "Track?"; read Track;;
-    7) 
-       echo "Car is primed and ready for the LFM Race!" 
+    7)
+       echo "Car is primed and ready for the LFM Race!"
        echo "Car?"; read Car;
        echo "Track?"; read Track;
        echo "Series Name?"; read SeriesName;;
-    *) 
+    *)
        echo "Invalid choice";;
 esac
 
-n=$(py "C:\Users\lucaz\Documents\Assetto Corsa Competizione\Setups\starts.py")
+n=$(python3 "starts.py")
 
 case $choice in
     1) echo "Welcome to the $SeriesName LFM Race with $Car on $Track! [#Race$n] #RoadTo1000";;
@@ -50,6 +51,6 @@ case $choice in
     6) echo "It's time for the LFM Race with $Car on $Track! [Race #$n] #RoadTo1000";;
     7) echo "$Car is primed and ready for the LFM Race at $Track! [Race #$n: $SeriesName] #RoadTo1000";;
     *) echo
-esac    
+esac
 
 read
